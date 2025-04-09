4370e17386555479c0ddd199dab0d915d825e24d
Pablo <287764@student.pwr.edu.pl>
2025-04-09 15:08:40 +0200

    fix: removed duplicate IntelliReserve folder

--- IntelliReserve/IntelliReserve/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IntelliReserve.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
