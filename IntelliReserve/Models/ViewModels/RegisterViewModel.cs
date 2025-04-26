d09dfec1907e98c4ba4fbd6b8daba94c6cfc17fe
RaulHoyoss <raulhzmgl@gmail.com>
2025-04-26 20:12:49 +0200

    subir branch raul

---
+++ IntelliReserve/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IntelliReserve.Models; // Esto es necesario para UserRole
+
+namespace IntelliReserve.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public UserRole? Role { get; set; }
+    }
+}
+
